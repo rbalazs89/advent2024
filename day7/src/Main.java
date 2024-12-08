64e6c7e76a3f4efa83ed7d85f857080db81ff08c
rbalazs89 <ritzinger.balazs@gmail.com>
2024-12-08 06:35:25 +0100

    day 7 part 2 done

--- day7/src/Main.java
+++ day7/src/Main.java
@@ -4,27 +4,77 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class Main {
-
     static ArrayList<ArrayList<String>> input = new ArrayList<>();
 
     public static void main(String[] args) {
         List<String> inputRaw = readFile("src/input1.txt");
         processFile(inputRaw);
         firstPart();
+        secondPart();
+    }
+    public static void secondPart(){
+        Long result = 0L;
+        for (int i = 0; i < input.size(); i++) {
+            result = result + processOneElementPart2(input.get(i));
+        }
+        System.out.println(result);
     }
 
     public static void firstPart(){
         Long result = 0L;
         for (int i = 0; i < input.size(); i++) {
-            result = result + processOneElement(input.get(i));
+            result = result + processOneElementPart1(input.get(i));
         }
         System.out.println(result);
     }
+    public static Long processOneElementPart2(ArrayList<String> oneLineInput){
+        int numCombinations = (int) Math.pow(3, oneLineInput.size() - 2);
+        String[] combinations = new String[numCombinations];
+        String item1 = "+";
+        String item2 = "*";
+        String item3 = "|";
 
-    public static Long processOneElement(ArrayList<String> oneLineInput){
+        // Generate combinations
+        for (int i = 0; i < numCombinations; i++) {
+            StringBuilder combination = new StringBuilder();
+            int value = i; // Use a temporary variable for base-3 conversion
+            for (int j = 0; j < oneLineInput.size() - 2; j++) {
+                int remainder = value % 3; // Get the current base-3 digit
+                value /= 3; // Move to the next base-3 digit
+
+                if (remainder == 0) {
+                    combination.append(item1);
+                } else if (remainder == 1) {
+                    combination.append(item2);
+                } else {
+                    combination.append(item3);
+                }
+            }
+            combinations[i] = combination.toString();
+        }
+
+        // generate number from combinations:
+        for(int i = 0; i < combinations.length; i ++){
+            Long tempResult = Long.parseLong(oneLineInput.get(1));
+            for (int j = 0; j < combinations[i].length(); j++) {
+                if(combinations[i].charAt(j) == '+'){
+                    tempResult = tempResult + Long.parseLong(oneLineInput.get(j + 2));
+                } else if (combinations[i].charAt(j) == '*'){
+                    tempResult = tempResult * Long.parseLong(oneLineInput.get(j + 2));
+                } else if (combinations[i].charAt(j) == '|'){
+                    tempResult = Long.parseLong(String.valueOf(tempResult)  + oneLineInput.get(j + 2));
+                }
+            }
+            if(tempResult == Long.parseLong(oneLineInput.get(0))){
+                return tempResult;
+            }
+        }
+        return 0L;
+    }
+
+    public static Long processOneElementPart1(ArrayList<String> oneLineInput){
         int numCombinations = (int) Math.pow(2, oneLineInput.size() - 2);
         String[] combinations = new String[numCombinations];
         String item1 = "+";
@@ -43,7 +93,6 @@ public class Main {
             combinations[i] = combination.toString();
         }
 
-
         // generate number from combinations:
         for(int i = 0; i < combinations.length; i ++){
             Long tempResult = Long.parseLong(oneLineInput.get(1));
@@ -60,7 +109,6 @@ public class Main {
         }
         return 0L;
     }
-
 
     public static void processFile(List<String> inputRaw) {
         for (String line : inputRaw) {
